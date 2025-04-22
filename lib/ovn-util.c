3077139d577ca81472a6451ba4ce2fd6258400f9
Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
2025-04-22 11:55:54 +0300

    controller: Added support for port mirroring in OVN overlay.
    Added new mirror port type which is the link between the source
    and the target port, its parent is the target port. VLAN tagging
    is not required for mirror ports — packets are transmitted without
    VLAN tag. Also since lport mirror works due to logical flows, we
    don't need to pass information about the mirror to ovs.
    
    Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
    Signed-off-by: Vladislav Odintsov <vlodintsov@k2.cloud>
    Co-authored-by: Vladislav Odintsov <vlodintsov@k2.cloud>
    Tested-by: Ivan Burnin <iburnin@k2.cloud>
    Acked-by: Numan Siddique <numans@ovn.org>
    (cherry picked from commit 6ccb350705cc32dbefe9a83e01b5a1cc4a1a7d84)
    Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>

--- lib/ovn-util.c
+++ lib/ovn-util.c
@@ -1180,6 +1180,8 @@ get_lport_type(const struct sbrec_port_binding *pb)
         return LP_REMOTE;
     } else if (!strcmp(pb->type, "vtep")) {
         return LP_VTEP;
+    } else if (!strcmp(pb->type, "mirror")) {
+        return LP_MIRROR;
     }
 
     return LP_UNKNOWN;
@@ -1193,6 +1195,8 @@ get_lport_type_str(enum en_lport_type lport_type)
         return "VIF";
     case LP_CONTAINER:
         return "CONTAINER";
+    case LP_MIRROR:
+        return "MIRROR";
     case LP_VIRTUAL:
         return "VIRTUAL";
     case LP_PATCH:
@@ -1234,6 +1238,7 @@ is_pb_router_type(const struct sbrec_port_binding *pb)
 
     case LP_VIF:
     case LP_CONTAINER:
+    case LP_MIRROR:
     case LP_VIRTUAL:
     case LP_LOCALNET:
     case LP_LOCALPORT:
