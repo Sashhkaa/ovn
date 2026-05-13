Sashhkaa/ovn

commit 6a52cfda703695be902076a0d0f9c9783506c96f

controller: Exclude ICMP frag-needed from VTEP tunnels.
Commit [1] added handling for E/W ICMPv4/v6 "fragmentation needed"
packets generated for overlay tunneled traffic. This was required
because the GENEVE kernel module generates ICMP "need frag" packets
when a tunneled packet exceeds the path MTU, and such packets were
previously dropped due to metadata needing to be swapped.

However, it did not cover the case where similar ICMP packets
arrive from VTEP tunnels. Such packets are not subject to the same
metadata handling constraints, since they are VXLAN-encapsulated and
are already delivered directly to the destination VIF MAC address.

As a result, they do not match the added for such packets rules and are
dropped. Exclude packets coming from VTEP tunnels from this special handling.

Fixes: 221476a01f26 ("ovn: Add tunnel PMTUD support.")
Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>

controller/encaps.c
@@ -52,6 +52,7 @@ encaps_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_name);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_type);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_options);
+    ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_other_config);
 }
 
 /* Enough context to create a new tunnel, using tunnel_add(). */
@@ -207,27 +208,30 @@ out:
 
 static void
 tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
-           const char *new_chassis_id, const struct sbrec_encap *encap,
+           const struct sbrec_chassis *chassis_rec,
+           const struct sbrec_encap *encap,
            bool must_set_local_ip, const char *local_ip,
            const struct ovsrec_open_vswitch_table *ovs_table)
 {
     struct smap options = SMAP_INITIALIZER(&options);
+    struct smap other_config = SMAP_INITIALIZER(&other_config);
     smap_add(&options, "remote_ip", encap->ip);
     smap_add(&options, "key", "flow");
     const char *dst_port = smap_get(&encap->options, "dst_port");
     const char *csum = smap_get(&encap->options, "csum");
     char *tunnel_entry_id = NULL;
     char *tunnel_entry_id_old = NULL;
-
+    bool is_vtep_tunnel = smap_get_bool(&chassis_rec->other_config,
+                                        "is-vtep", false);
     /*
      * Since a chassis may have multiple encap-ip, we can't just add the
      * chassis name as the OVN_TUNNEL_ID for the port; we use the
      * combination of the chassis_name and the remote and local encap-ips to
      * identify a specific tunnel to the remote chassis.
      */
-    tunnel_entry_id = encaps_tunnel_id_create(new_chassis_id, encap->ip,
+    tunnel_entry_id = encaps_tunnel_id_create(chassis_rec->name, encap->ip,
                                               local_ip);
-    tunnel_entry_id_old = encaps_tunnel_id_create_legacy(new_chassis_id,
+    tunnel_entry_id_old = encaps_tunnel_id_create_legacy(chassis_rec->name,
                                                          encap->ip);
     if (csum && (!strcmp(csum, "true") || !strcmp(csum, "false"))) {
         smap_add(&options, "csum", csum);
@@ -272,7 +276,7 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
     /* Add auth info if ipsec is enabled. */
     if (sbg->ipsec) {
         set_local_ip = true;
-        smap_add(&options, "remote_name", new_chassis_id);
+        smap_add(&options, "remote_name", chassis_rec->name);
 
         /* Force NAT-T traversal via configuration */
         /* Two ipsec backends are supported: libreswan and strongswan */
@@ -292,6 +296,10 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
 
     if (set_local_ip) {
         smap_add(&options, "local_ip", local_ip);
+    }
+
+    if (is_vtep_tunnel) {
+        smap_add(&other_config, "is-vtep", "true");
     }
 
     /* If there's an existing tunnel record that does not need any change,
@@ -330,10 +338,10 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
      * its name, otherwise generate a new, unique name. */
     char *port_name = (tunnel
                        ? xstrdup(tunnel->port->name)
-                       : tunnel_create_name(tc, new_chassis_id));
+                       : tunnel_create_name(tc, chassis_rec->name));
     if (!port_name) {
         VLOG_WARN("Unable to allocate unique name for '%s' tunnel",
-                  new_chassis_id);
+                  chassis_rec->name);
         goto exit;
     }
 
@@ -341,6 +349,7 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
     ovsrec_interface_set_name(iface, port_name);
     ovsrec_interface_set_type(iface, encap->type);
     ovsrec_interface_set_options(iface, &options);
+    ovsrec_interface_set_other_config(iface, &other_config);
 
     struct ovsrec_port *port = ovsrec_port_insert(tc->ovs_txn);
     ovsrec_port_set_name(port, port_name);
@@ -356,6 +365,7 @@ exit:
     free(tunnel_entry_id);
     free(tunnel_entry_id_old);
     smap_destroy(&options);
+    smap_destroy(&other_config);
 }
 
 static bool
@@ -430,7 +440,7 @@ chassis_tunnel_add(const struct sbrec_chassis *chassis_rec,
             }
             VLOG_DBG("tunnel_add: '%s', local ip: %s", chassis_rec->name,
                      this_chassis->encaps[j]->ip);
-            tunnel_add(tc, sbg, chassis_rec->name, chassis_rec->encaps[i],
+            tunnel_add(tc, sbg, chassis_rec, chassis_rec->encaps[i],
                        set_local_ip, this_chassis->encaps[j]->ip, ovs_table);
             tuncnt++;
         }

controller/local_data.c
@@ -463,7 +463,8 @@ local_nonvif_data_run(const struct ovsrec_bridge *br_int,
                 tun->ofport = u16_to_ofp(ofport);
                 tun->type = tunnel_type;
                 tun->is_ipv6 = ip ? addr_is_ipv6(ip) : false;
-
+                tun->is_vtep_tunnel = smap_get_bool(&iface_rec->other_config,
+                                                    "is-vtep", false);
                 free(hash_id);
                 free(ip);
                 break;

controller/local_data.h
@@ -141,6 +141,7 @@ struct chassis_tunnel {
     ofp_port_t ofport;
     enum chassis_tunnel_type type;
     bool is_ipv6;
+    bool is_vtep_tunnel;
 };
 
 void local_nonvif_data_run(const struct ovsrec_bridge *br_int,

controller/physical.c
@@ -2574,34 +2574,46 @@ physical_run(struct physical_ctx *p_ctx,
         ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 100, 0, &match,
                         &ofpacts, hc_uuid);
 
-        /* Set allow rx from tunnel bit. */
-        put_load(1, MFF_LOG_FLAGS, MLF_RX_FROM_TUNNEL_BIT, 1, &ofpacts);
-
-        /* Add specif flows for E/W ICMPv{4,6} packets if tunnelled packets
-         * do not fit path MTU.
+        /* Add handling for E/W ICMPv4/v6 packets when tunneled packets exceed
+         * path MTU.
+         * If packet needs to be tunneled to another node and the physical
+         * interface used for tunneling has a lower MTU than the packet size,
+         * or if there is a route exception with a smaller MTU, kernel
+         * generates an ICMP "Fragmentation Needed" message, but the package
+         * metadata didn't change. Such packets might have been dropped due
+         * to required metadata modifications for returned packet.
+         *
+         * Mark these packets with MLF_RX_FROM_TUNNEL_BIT for further
+         * processing. Packets received from a VTEP tunnel should be passed
+         * through, and errors handled via the normal processing path, since
+         * port metadata is not carried in VTEP packets in VNI.
          */
-        put_resubmit(OFTABLE_CT_ZONE_LOOKUP, &ofpacts);
+        if (!tun->is_vtep_tunnel) {
+            put_load(1, MFF_LOG_FLAGS, MLF_RX_FROM_TUNNEL_BIT, 1, &ofpacts);
 
-        /* IPv4 */
-        match_init_catchall(&match);
-        match_set_in_port(&match, tun->ofport);
-        match_set_dl_type(&match, htons(ETH_TYPE_IP));
-        match_set_nw_proto(&match, IPPROTO_ICMP);
-        match_set_icmp_type(&match, 3);
-        match_set_icmp_code(&match, 4);
+            put_resubmit(OFTABLE_CT_ZONE_LOOKUP, &ofpacts);
 
-        ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 120, 0, &match,
-                        &ofpacts, hc_uuid);
-        /* IPv6 */
-        match_init_catchall(&match);
-        match_set_in_port(&match, tun->ofport);
-        match_set_dl_type(&match, htons(ETH_TYPE_IPV6));
-        match_set_nw_proto(&match, IPPROTO_ICMPV6);
-        match_set_icmp_type(&match, 2);
-        match_set_icmp_code(&match, 0);
+            /* IPv4 */
+            match_init_catchall(&match);
+            match_set_in_port(&match, tun->ofport);
+            match_set_dl_type(&match, htons(ETH_TYPE_IP));
+            match_set_nw_proto(&match, IPPROTO_ICMP);
+            match_set_icmp_type(&match, 3);
+            match_set_icmp_code(&match, 4);
 
-        ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 120, 0, &match,
-                        &ofpacts, hc_uuid);
+            ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 120, 0, &match,
+                            &ofpacts, hc_uuid);
+            /* IPv6 */
+            match_init_catchall(&match);
+            match_set_in_port(&match, tun->ofport);
+            match_set_dl_type(&match, htons(ETH_TYPE_IPV6));
+            match_set_nw_proto(&match, IPPROTO_ICMPV6);
+            match_set_icmp_type(&match, 2);
+            match_set_icmp_code(&match, 0);
+
+            ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 120, 0, &match,
+                            &ofpacts, hc_uuid);
+        }
     }
 
     /* Add VXLAN specific rules to transform port keys

tests/ovn-controller-vtep.at
@@ -628,6 +628,10 @@ AT_CHECK([ovs-ofctl dump-flows br-int table=OFTABLE_PHY_TO_LOG | grep 'priority=
 priority=110,tun_id=0x<>,in_port=<> actions=move:NXM_NX_TUN_ID[[0..23]]->OXM_OF_METADATA[[0..23]],load:0x<>->NXM_NX_REG14[[0..14]],load:0x<>->NXM_NX_REG10[[1]],resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
 ])
 
+# Skip processing ICMP "packet too big" errors in this table if the packet came from a VTEP tunnel.
+AT_CHECK([ovs-ofctl dump-flows br-int table=OFTABLE_PHY_TO_LOG | \
+          grep -E 'icmp_type=3,icmp_code=4|icmp_type=2,icmp_code=0'], [1], [])
+
 OVN_CLEANUP([hv1])
 AT_CLEANUP
 ])

commit cc4c279070e2762fc59b25633b6ecf793a60eb5a

controller: Fix is-vtep-tunnel detection on first controller start.
The is_vtep_tunnel flag in chassis_tunnel was read from the
other_config column in the interface table. This column is populated
by encaps_run() via an OVSDB transaction, but at the time
local_nonvif_data_run() reads it, the transaction may not yet have
been committed back to the IDL cache, resulting in is_vtep_tunnel
always being false on the first run.

Fix this by falling back to reading the is-vtep flag directly from
the remote chassis record in the Southbound DB when
iface->other_config is not yet populated.

Fixes: 6a52cfda7036 ("controller: Exclude ICMP frag-needed from VTEP tunnels.")
Signed-off-by: Alexandra Rukomoinikova <ARukomoinikova@k2.cloud>

controller/encaps.c
@@ -52,7 +52,6 @@ encaps_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_name);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_type);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_options);
-    ovsdb_idl_track_add_column(ovs_idl, &ovsrec_interface_col_other_config);
 }
 
 /* Enough context to create a new tunnel, using tunnel_add(). */
@@ -214,15 +213,12 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
            const struct ovsrec_open_vswitch_table *ovs_table)
 {
     struct smap options = SMAP_INITIALIZER(&options);
-    struct smap other_config = SMAP_INITIALIZER(&other_config);
     smap_add(&options, "remote_ip", encap->ip);
     smap_add(&options, "key", "flow");
     const char *dst_port = smap_get(&encap->options, "dst_port");
     const char *csum = smap_get(&encap->options, "csum");
     char *tunnel_entry_id = NULL;
     char *tunnel_entry_id_old = NULL;
-    bool is_vtep_tunnel = smap_get_bool(&chassis_rec->other_config,
-                                        "is-vtep", false);
     /*
      * Since a chassis may have multiple encap-ip, we can't just add the
      * chassis name as the OVN_TUNNEL_ID for the port; we use the
@@ -298,10 +294,6 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
         smap_add(&options, "local_ip", local_ip);
     }
 
-    if (is_vtep_tunnel) {
-        smap_add(&other_config, "is-vtep", "true");
-    }
-
     /* If there's an existing tunnel record that does not need any change,
      * keep it.  Otherwise, create a new record (if there was an existing
      * record, the new record will supplant it and encaps_run() will delete
@@ -349,7 +341,6 @@ tunnel_add(struct tunnel_ctx *tc, const struct sbrec_sb_global *sbg,
     ovsrec_interface_set_name(iface, port_name);
     ovsrec_interface_set_type(iface, encap->type);
     ovsrec_interface_set_options(iface, &options);
-    ovsrec_interface_set_other_config(iface, &other_config);
 
     struct ovsrec_port *port = ovsrec_port_insert(tc->ovs_txn);
     ovsrec_port_set_name(port, port_name);
@@ -365,7 +356,6 @@ exit:
     free(tunnel_entry_id);
     free(tunnel_entry_id_old);
     smap_destroy(&options);
-    smap_destroy(&other_config);
 }
 
 static bool

controller/local_data.c
@@ -31,6 +31,7 @@
 #include "lib/ovn-util.h"
 #include "lib/ovn-sb-idl.h"
 #include "local_data.h"
+#include "lib/chassis-index.h"
 #include "lport.h"
 
 VLOG_DEFINE_THIS_MODULE(ldata);
@@ -389,6 +390,7 @@ tracked_datapaths_destroy(struct hmap *tracked_datapaths)
 void
 local_nonvif_data_run(const struct ovsrec_bridge *br_int,
                       const struct sbrec_chassis *chassis_rec,
+                      struct ovsdb_idl_index *sbrec_chassis_by_name,
                       struct simap *patch_ofports,
                       struct hmap *chassis_tunnels)
 {
@@ -456,6 +458,8 @@ local_nonvif_data_run(const struct ovsrec_bridge *br_int,
                 if (!encaps_tunnel_id_parse(tunnel_id, &hash_id, &ip, NULL)) {
                     continue;
                 }
+                const struct sbrec_chassis *chassis
+                    = chassis_lookup_by_name(sbrec_chassis_by_name, hash_id);
                 struct chassis_tunnel *tun = xmalloc(sizeof *tun);
                 hmap_insert(chassis_tunnels, &tun->hmap_node,
                             hash_string(hash_id, 0));
@@ -463,8 +467,10 @@ local_nonvif_data_run(const struct ovsrec_bridge *br_int,
                 tun->ofport = u16_to_ofp(ofport);
                 tun->type = tunnel_type;
                 tun->is_ipv6 = ip ? addr_is_ipv6(ip) : false;
-                tun->is_vtep_tunnel = smap_get_bool(&iface_rec->other_config,
-                                                    "is-vtep", false);
+                if (chassis) {
+                    tun->is_vtep_tunnel = smap_get_bool(&chassis->other_config,
+                                                        "is-vtep", false);
+                }
                 free(hash_id);
                 free(ip);
                 break;

controller/local_data.h
@@ -146,6 +146,7 @@ struct chassis_tunnel {
 
 void local_nonvif_data_run(const struct ovsrec_bridge *br_int,
                            const struct sbrec_chassis *,
+                           struct ovsdb_idl_index * sbrec_chassis_by_name,
                            struct simap *patch_ofports,
                            struct hmap *chassis_tunnels);
 

controller/ovn-controller.c
@@ -3769,7 +3769,8 @@ en_non_vif_data_run(struct engine_node *node, void *data)
         = chassis_lookup_by_name(sbrec_chassis_by_name, chassis_id);
     ovs_assert(chassis);
 
-    local_nonvif_data_run(br_int, chassis, &ed_non_vif_data->patch_ofports,
+    local_nonvif_data_run(br_int, chassis, sbrec_chassis_by_name,
+                          &ed_non_vif_data->patch_ofports,
                           &ed_non_vif_data->chassis_tunnels);
     engine_set_node_state(node, EN_UPDATED);
 }