8559303031b4b1bd370b3da843c5da921d94e00b
Alexandra Rukomoinikova <ARukomoinikova@k2.cloud>
2026-07-26 20:01:59 +0300

    Revert "treewide: Remove the deprecated ct_lb action."
    This reverts commit d935db5c503f72a0a648b3e26d08f171960a524a.

--- lib/ovn-util.c
+++ lib/ovn-util.c
@@ -1007,8 +1007,8 @@ ip_address_and_port_from_lb_key(const char *key, char **ip_address,
  *
  * NOTE: If OVN_NORTHD_PIPELINE_CSUM is updated make sure to double check
  * whether an update of OVN_INTERNAL_MINOR_VER is required. */
-#define OVN_NORTHD_PIPELINE_CSUM "3917613301 11397"
-#define OVN_INTERNAL_MINOR_VER 16
+#define OVN_NORTHD_PIPELINE_CSUM "2814004238 11453"
+#define OVN_INTERNAL_MINOR_VER 17
 
 /* Returns the OVN version. The caller must free the returned value. */
 char *
