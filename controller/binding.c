Sashhkaa/ovn

commit 16734b82f207d29ed71ee1c9f8ad5b9fa0884e1f

ovn-util: Refactor chassis_tunnel_type enum to use sequential values.
Change enum chassis_tunnel_type from bit-field values to simple
sequential indices to simplify array-based lookups in future
optimizations.

The preference order (GENEVE > VXLAN) is preserved through numeric
comparison.

Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Ales Musil <amusil@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>

controller/encaps.c
@@ -349,7 +349,9 @@ exit:
 }
 
 static bool
-chassis_has_type(const struct sbrec_chassis *chassis, uint32_t tun_type) {
+chassis_has_type(const struct sbrec_chassis *chassis,
+                 enum chassis_tunnel_type tun_type)
+{
     for (size_t i = 0; i < chassis->n_encaps; i++) {
         if (get_tunnel_type(chassis->encaps[i]->type) == tun_type) {
             return true;
@@ -363,10 +365,11 @@ preferred_encap(const struct sbrec_chassis *chassis_rec,
                 const struct sbrec_chassis *this_chassis)
 {
     struct sbrec_encap *best_encap = NULL;
-    uint32_t best_type = 0;
+    enum chassis_tunnel_type best_type = TUNNEL_TYPE_INVALID;
 
     for (size_t i = 0; i < chassis_rec->n_encaps; i++) {
-        uint32_t tun_type = get_tunnel_type(chassis_rec->encaps[i]->type);
+        enum chassis_tunnel_type tun_type =
+            get_tunnel_type(chassis_rec->encaps[i]->type);
         if (tun_type > best_type && chassis_has_type(this_chassis, tun_type)) {
             best_type = tun_type;
             best_encap = chassis_rec->encaps[i];

controller/ovn-controller.h
@@ -24,6 +24,6 @@ struct ovsrec_bridge_table;
 const struct ovsrec_bridge *get_bridge(const struct ovsrec_bridge_table *,
                                        const char *br_name);
 
-uint32_t get_tunnel_type(const char *name);
+enum chassis_tunnel_type get_tunnel_type(const char *name);
 
 #endif /* controller/ovn-controller.h */

lib/ovn-util.c
@@ -946,7 +946,7 @@ ovn_get_internal_version(void)
                      N_OVNACTS, OVN_INTERNAL_MINOR_VER);
 }
 
-uint32_t
+enum chassis_tunnel_type
 get_tunnel_type(const char *name)
 {
     if (!strcmp(name, "geneve")) {
@@ -955,7 +955,7 @@ get_tunnel_type(const char *name)
         return VXLAN;
     }
 
-    return 0;
+    return TUNNEL_TYPE_INVALID;
 }
 
 const struct ovsrec_bridge *

lib/ovn-util.h
@@ -345,14 +345,15 @@ hash_add_in6_addr(uint32_t hash, const struct in6_addr *addr)
     return hash;
 }
 
-/* Must be a bit-field ordered from most-preferred (higher number) to
+/* Tunnel types ordered from most-preferred (higher number) to
  * least-preferred (lower number). */
 enum chassis_tunnel_type {
-    GENEVE = 1 << 1,
-    VXLAN  = 1 << 0
+    TUNNEL_TYPE_INVALID = -1,
+    VXLAN  = 0,
+    GENEVE = 1
 };
 
-uint32_t get_tunnel_type(const char *name);
+enum chassis_tunnel_type get_tunnel_type(const char *name);
 
 struct ovsrec_bridge_table;
 const struct ovsrec_bridge *get_bridge(const struct ovsrec_bridge_table *,

commit 549ac8bdf3556e6e52fdd9fb56ed778323c90d82

controller: Fix tunnel type checks broken by sequential enum values.
The commit being fixed change enum chassis_tunnel_type from bit-field
values to simple sequential indices to simplify array-based lookups
in future optimizations but some callers were left unchanged this led
to incorrect logging.

Fixes: 16734b82f207 ("ovn-util: Refactor chassis_tunnel_type enum to use sequential values.")
Signed-off-by: Alexandra Rukomoinikova <ARukomoinikova@k2.cloud>

controller/binding.c
@@ -554,12 +554,13 @@ sbrec_get_port_encap(const struct sbrec_chassis *chassis_rec,
     }
 
     struct sbrec_encap *best_encap = NULL;
-    uint32_t best_type = 0;
+    enum chassis_tunnel_type best_type = TUNNEL_TYPE_INVALID;
     for (int i = 0; i < chassis_rec->n_encaps; i++) {
         if ((encap_ip && !strcmp(chassis_rec->encaps[i]->ip, encap_ip)) ||
             (!encap_ip && smap_get_bool(&chassis_rec->encaps[i]->options,
                                         "is_default", false))) {
-            uint32_t tun_type = get_tunnel_type(chassis_rec->encaps[i]->type);
+            enum chassis_tunnel_type tun_type =
+                get_tunnel_type(chassis_rec->encaps[i]->type);
             if (tun_type > best_type) {
                 best_type = tun_type;
                 best_encap = chassis_rec->encaps[i];

controller/chassis.c
@@ -270,9 +270,9 @@ chassis_parse_ovs_encap_type(const char *encap_type,
     const char *type;
 
     SSET_FOR_EACH (type, encap_type_set) {
-        uint32_t tun_type = get_tunnel_type(type);
+        enum chassis_tunnel_type tun_type = get_tunnel_type(type);
 
-        if (!tun_type) {
+        if (tun_type == TUNNEL_TYPE_INVALID) {
             VLOG_INFO_RL(&rl, "Unknown tunnel type: %s", type);
         }
     }

controller/encaps.c
@@ -398,9 +398,10 @@ chassis_tunnel_add(const struct sbrec_chassis *chassis_rec,
         return tuncnt;
     }
 
-    uint32_t pref_type = get_tunnel_type(encap->type);
+    enum chassis_tunnel_type pref_type = get_tunnel_type(encap->type);
     for (int i = 0; i < chassis_rec->n_encaps; i++) {
-        uint32_t tun_type = get_tunnel_type(chassis_rec->encaps[i]->type);
+        enum chassis_tunnel_type tun_type =
+            get_tunnel_type(chassis_rec->encaps[i]->type);
         if (tun_type != pref_type) {
             continue;
         }